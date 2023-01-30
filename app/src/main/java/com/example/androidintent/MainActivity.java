6c434581032444de1e423fe7fae1904a612171ad
sumanth <saisumanth5318@gmail.com>
2023-01-30 18:46:17 -0500

    intent done

---
+++ app/src/main/java/com/example/androidintent/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.androidintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1,b2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(android.content.Intent.ACTION_VIEW,
+                        Uri.parse("http://www.youtube.com"));
+                startActivity(i);
+            }
+        });
+
+        b2=(Button)findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(android.content.Intent.ACTION_VIEW,
+                        Uri.parse("content://contacts/people/prasanth"));
+                startActivity(i);
+            }
+        });
+    }
+}
